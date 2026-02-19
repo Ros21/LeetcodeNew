276d663df225e81e3275b6fa58646fdcafd3a5cc
roshanpandey <11rshan@gmail.com>
2026-02-19 11:02:11 +0545

    Solve power of 4 leetcode

--- power_of_four.py
+++ power_of_four.py
@@ -3,13 +3,13 @@ import math
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        pass
+        return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
 
         
-print(Solution().isPowerOfFour(16))  # Example usage
-print(Solution().isPowerOfFour(5))   # Example usage
-print(Solution().isPowerOfFour(1))   # Example usage
-print(Solution().isPowerOfFour(0))   # Example usage
-print(Solution().isPowerOfFour(-4))  # Example usage
-print(Solution().isPowerOfFour(8))  # Example usage
-print(Solution().isPowerOfFour(4))  # Example usage
+print(Solution().isPowerOfFour(16))  # True
+print(Solution().isPowerOfFour(5))   # False
+print(Solution().isPowerOfFour(1))   # True
+print(Solution().isPowerOfFour(0))   # False
+print(Solution().isPowerOfFour(-4))  # False
+print(Solution().isPowerOfFour(8))  # False
+print(Solution().isPowerOfFour(4))  # True
